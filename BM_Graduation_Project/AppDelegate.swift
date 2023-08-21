22f83503f4af689fd7d9bd4db3ec9aac59b17114
Mohamed-Elsemary <mohamedelsemary19@yahoo.com>
2023-08-21 12:58:34 +0300

    Let's start

---
+++ BM_Graduation_Project/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BM_Graduation_Project
+//
+//  Created by mohamed on 21/08/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+}
+
